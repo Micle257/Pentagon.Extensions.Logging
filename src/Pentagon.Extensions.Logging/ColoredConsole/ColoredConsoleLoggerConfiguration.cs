8a02b4eea29e8ec175434bd2d0ba79fc9bdc86c6
Michal Pokorný <michal.pokorny0@gmail.com>
2019-04-17 09:23:09 +0200

    add console logger

---
+++ src/Pentagon.Extensions.Logging/ColoredConsole/ColoredConsoleLoggerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ILoggerExtensions.cs">
+//   Copyright (c) Smartdata s.r.o. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Extensions.Logging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using Microsoft.Extensions.Logging;
+
+    public class ColoredConsoleLoggerConfiguration
+    {
+        public IDictionary<LogLevel, (ConsoleColor Foreground, ConsoleColor Background)> Colors { get; set; } =
+            new Dictionary<LogLevel, (ConsoleColor Foreground, ConsoleColor Background)>
+            {
+                    {LogLevel.Critical, (ConsoleColor.White, ConsoleColor.Red) },
+                    {LogLevel.Error, (ConsoleColor.Red, ConsoleColor.Black) },
+                    {LogLevel.Warning,  (ConsoleColor.Yellow, ConsoleColor.Black)},
+                    {LogLevel.Information,  (ConsoleColor.Blue, ConsoleColor.Black)},
+                    {LogLevel.Debug,  (ConsoleColor.White, ConsoleColor.Black)},
+                    {LogLevel.Trace,  (ConsoleColor.Gray, ConsoleColor.Black)}
+            };
+    }
+}
---
+++ src/Pentagon.Extensions.Logging/ColoredConsole/ColoredConsoleLoggerExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace Pentagon.Extensions.Logging {
+    using System;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    public static class ColoredConsoleLoggerExtensions
+    {
+        public static ILoggerFactory AddColoredConsoleLogger(this ILoggerFactory loggerFactory, ColoredConsoleLoggerConfiguration config)
+        {
+            loggerFactory.AddProvider(new ColoredConsoleLoggerProvider(config));
+
+            return loggerFactory;
+        }
+
+        public static ILoggerFactory AddColoredConsoleLogger(this ILoggerFactory loggerFactory)
+        {
+            var config = new ColoredConsoleLoggerConfiguration();
+
+            return loggerFactory.AddColoredConsoleLogger(config);
+        }
+
+        public static ILoggerFactory AddColoredConsoleLogger(this ILoggerFactory loggerFactory, Action<ColoredConsoleLoggerConfiguration> configure)
+        {
+            var config = new ColoredConsoleLoggerConfiguration();
+
+            configure?.Invoke(config);
+
+            return loggerFactory.AddColoredConsoleLogger(config);
+        }
+
+        public static ILoggingBuilder AddColoredConsole(this ILoggingBuilder loggerFactory, ColoredConsoleLoggerConfiguration config)
+        {
+            loggerFactory.AddProvider(new ColoredConsoleLoggerProvider(config));
+
+            return loggerFactory;
+        }
+
+        public static ILoggingBuilder AddColoredConsole(this ILoggingBuilder loggerFactory)
+        {
+            var config = new ColoredConsoleLoggerConfiguration();
+
+            return loggerFactory.AddColoredConsole(config);
+        }
+
+        public static ILoggingBuilder AddColoredConsole(this ILoggingBuilder loggerFactory, Action<ColoredConsoleLoggerConfiguration> configure)
+        {
+            var config = new ColoredConsoleLoggerConfiguration();
+
+            configure?.Invoke(config);
+
+            return loggerFactory.AddColoredConsole(config);
+        }
+    }
+}
